029d3e76471a5d3db09de5a66221f576ec271d6c
gondrongafri <63855592+gondrongafri@users.noreply.github.com>
2020-06-10 18:31:31 +0700

    Update yogi.php

--- yogi.php
+++ yogi.php
@@ -98,7 +98,7 @@ echo "\e[89m▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬�
         echo color("blue",".");
         sleep(23);
         }
-        $code1 = request('/go-promotions/v1/promotions/enrollments', $token, '{"promo_code":"COBAGOFOOD260520"}');
+        $code1 = request('/go-promotions/v1/promotions/enrollments', $token, '{"promo_code":"COBAGOFOOD0906"}');
         $message = fetch_value($code1,'"message":"','"');
         if(strpos($code1, 'Promo kamu sudah bisa dipakai')){
         echo "\n".color("pink","🔓▶️ Message: ".$message);
